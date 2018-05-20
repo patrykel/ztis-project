ea1be00dee03b595ec8d4aa02d972f79f71546ad
regis <regis128>
2018-05-20 15:03:42 +0200

    dodany skrypt dający notki

--- src/fxa/scrapers/news_api.py
+++ src/fxa/scrapers/news_api.py
@@ -1,8 +1,13 @@
 from newsapi import NewsApiClient
 import scrapy
 import datetime
+import csv
 
-newsapi = NewsApiClient(api_key='791699a09e634365b7edca7815029d6c')
+
+CURRENCY = 'euro'
+
+# newsapi = NewsApiClient(api_key='791699a09e634365b7edca7815029d6c')
+newsapi = NewsApiClient(api_key='81a67c69e5f64233b124075faec2bc96')
 
 
 def parse_date(date):
@@ -11,17 +16,24 @@ def parse_date(date):
 
 
 class NewsScraper(scrapy.Spider):
-    all_articles = newsapi.get_everything(q='euro',
-                                          language='en',
-                                          sort_by='relevancy')
+    myFile = open(CURRENCY + '.csv', 'a')
+    writer = csv.writer(myFile)
 
-    for i in range(all_articles['totalResults'] / 20):
-        partial_articles = newsapi.get_everything(q='euro',
+    all_articles = newsapi.get_everything(q=CURRENCY,
+                                          language='en')
+
+    # print(all_articles['totalResults'])
+    for i in range(499, int(all_articles['totalResults'] / 20)):
+        print(i)
+        partial_articles = newsapi.get_everything(q=CURRENCY,
                                                   language='en',
-                                                  sort_by='relevancy',
+                                                  sort_by='publishedAt',
                                                   page=i+1)
 
         # tutaj zmienić żeby dostosować do kodu, poniższe linie wypiszę wszystkie notki z zapytania wyżej w formacie
         # odpowiadającym plikowi rss_unique.csv w data/notes
-        #for article in all_articles['articles']:
+        for article in partial_articles['articles']:
+            writer.writerows([["en_news_api", parse_date(article['publishedAt']), article['title'], article['description']]])
             #print("en_news_api" + " | " + parse_date(article['publishedAt']) + " | " + article['title'] + " | " + article['description'])
+
+    myFile.close()
